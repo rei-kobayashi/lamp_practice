6b43fa79bf2cc45c518f9953c6f965c1dcc068c0
[Rei <[rei5562007@gmail.com]>
2020-02-01 20:43:47 +0900

    csrf対応 #4

--- www/html/index_add_cart.php
+++ www/html/index_add_cart.php
@@ -7,20 +7,23 @@ require_once MODEL_PATH . 'cart.php';
 
 session_start();
 
-if(is_logined() === false){
+if (is_logined() === false) {
   redirect_to(LOGIN_URL);
 }
 
 $db = get_db_connect();
 $user = get_login_user($db);
+$token = get_post('token');
+if (is_valid_csrf_token($token) === TRUE) {
+  $item_id = get_post('item_id');
 
-
-$item_id = get_post('item_id');
-
-if(add_cart($db,$user['user_id'], $item_id)){
-  set_message('カートに商品を追加しました。');
-} else {
-  set_error('カートの更新に失敗しました。');
+  if (add_cart($db, $user['user_id'], $item_id)) {
+    set_message('カートに商品を追加しました。');
+  } else {
+    set_error('カートの更新に失敗しました。');
+  }
+}else{
+  set_error('不正な操作が行われました。');
 }
 
 redirect_to(HOME_URL);
